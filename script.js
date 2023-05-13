c9e1e1512c9b8e6537d1c6b5c8c7b410fdeceb97
padawanNG <123118074+padawanNG@users.noreply.github.com>
2023-05-13 19:58:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+// Récupération du fichier soumis par l'utilisateur
 const fileInput = document.getElementById('fileInput');
-const submitButton = document.getElementById('submitButton');
+const file = fileInput.files[0];
 
-submitButton.addEventListener('click', () => {
-  const file = fileInput.files[0];
-  if (file && file.name.endsWith('.py')) {
-    const formData = new FormData();
-    formData.append('file', file);
-    fetch('/submitted-files', {
-      method: 'POST',
-      body: formData
-    })
+// Création du contenu du fichier
+const content = "Contenu du fichier à envoyer sur GitHub";
+
+// Configuration de l'objet qui contiendra les données à envoyer
+const data = {
+  message: 'Ajout d\'un nouveau fichier',
+  content: btoa(content),
+  branch: 'main'
+};
+
+// Envoi de la requête POST à l'API GitHub
+fetch('https://api.github.com/repos/padawanNG/Numworks_games/contents/submitted-files/file.py', {
+  method: 'PUT',
+  headers: {
+    Authorization: 'token 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',
+    Accept: 'application/vnd.github.v3+json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(data)
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
