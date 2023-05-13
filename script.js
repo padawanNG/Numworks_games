bca197d55e729253112cfe54589ffa21bd117cd3
padawanNG <123118074+padawanNG@users.noreply.github.com>
2023-05-13 20:13:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-// Récupération du fichier soumis par l'utilisateur
-const fileInput = document.getElementById('fileInput');
-const file = fileInput.files[0];
+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
 
-// Création du contenu du fichier
-const content = "Contenu du fichier à envoyer sur GitHub";
+function submitFile() {
+  const fileInput = document.getElementById('fileInput');
+  const file = fileInput.files[0];
+  if (file && file.name.endsWith('.py')) {
+    const formData = new FormData();
+    formData.append('file', file);
+    fetch('https://uploads.github.com/repos/PadawanNG/Numworks_games/git/blobs', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'token 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',
+        'Content-Type': 'text/plain'
+      },
+      body: file
+    })
+    .then(response => response.json())
+    .then(data => {
+      const blobSha = data.sha;
+      const path = 'submitted-files/' + file.name;
+      fetch('https://api.github.com/repos/oadawanNG/Numworks_games/contents/' + path, {
+        method: 'PUT',
+        headers: {
+          'Authorization': 'token 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',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          message: 'Ajout d\'un fichier ' + file.name,
+          content: blobSha,
+          path: path,
+          branch: 'main'
+        })
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
+  }
+}
 
-// Configuration de l'objet qui contiendra les données à envoyer
-const data = {
-  message: 'Ajout d\'un nouveau fichier',
-  content: btoa(content),
-  branch: 'main'
-};
-
-// Envoi de la requête POST à l'API GitHub
-fetch('https://api.github.com/repos/padawanNG/Numworks_games/contents/submitted-files/file.py', {
-  method: 'PUT',
-  headers: {
-    Authorization: 'token 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',
-    Accept: 'application/vnd.github.v3+json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(data)
-})
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
