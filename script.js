ee66e29d0d7e69be57e2b25b1808dc074f259144
padawanNG <123118074+padawanNG@users.noreply.github.com>
2023-05-14 12:09:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
-import requests
+function submitFile() {
+  // Récupérer le fichier déposé par l'utilisateur
+  const file = document.getElementById('fileInput').files[0];
 
-def submitFile():
-    file = document.getElementById("fileInput").files[0]
-    filename = file.name
-    with open(filename, 'rb') as data:
-        response = requests.put(
-            'https://upload.github.com/repos/padawanNG/Numworks_games/contents/submitted-files/{}'.format(filename),
-            headers={'Authorization': 'token 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'},
-            data=data
-        )
-    if response.status_code == 201:
-        print('Le fichier a été soumis avec succès !')
-    else:
-        print('Erreur lors de la soumission du fichier.')
+  // Créer un objet FormData contenant le fichier
+  const formData = new FormData();
+  formData.append('file', file);
+
+  // Configurer la requête pour créer un nouveau commit avec le fichier
+  const repoName = 'Numworks_games';
+  const username = 'padawanNG';
+  const accessToken = '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';
+  const commitMessage = 'Ajout d\'un nouveau fichier';
+  const branchName = 'main';
+  const filePath = 'submitted-files/' + file.name;
+
+  const url = `https://api.github.com/repos/${username}/${repoName}/contents/${filePath}`;
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    body: JSON.stringify({
+      message: commitMessage,
+      content: btoa(file),
+      branch: branchName,
+      path: filePath
+    })
+  };
+
+  // Envoyer la requête pour créer le nouveau commit
+  fetch(url, options)
+    .then(response => {
+      if (response.ok) {
+        alert('Le fichier a été ajouté avec succès!');
+      } else {
+        alert('Une erreur est survenue lors de l\'ajout du fichier.');
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      alert('Une erreur est survenue lors de l\'ajout du fichier.');
+    });
+}
+
+module.exports = {
+  submitFile
+}
