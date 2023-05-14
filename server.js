e7d964f1ed149d1cafc238d95374221a98910bd2
padawanNG <123118074+padawanNG@users.noreply.github.com>
2023-05-14 16:00:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-function submitFile() {
-  const fileInput = document.getElementById('fileInput');
-  const file = fileInput.files[0];
-  const formData = new FormData();
-  formData.append('file', file);
+const express = require('express');
+const fileUpload = require('express-fileupload');
 
-  axios.post('https://file.io/?expires=1d', formData)
-    .then(response => {
-      const downloadLink = response.data.link;
-      window.location.href = 'confirmation.html?link=' + encodeURIComponent(downloadLink);
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}
+const app = express();
 
+// Activer l'upload de fichiers
+app.use(fileUpload());
+
+// Définir la route pour la soumission de fichiers
+app.post('/submit-file', (req, res) => {
+  // Vérifier si des fichiers ont été soumis
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('Aucun fichier n\'a été soumis.');
+  }
+
+  // Récupérer le fichier soumis
+  const submittedFile = req.files.submittedFile;
+
+  // Enregistrer le fichier dans le dossier "submitted-files"
+  submittedFile.mv('submitted-files/' + submittedFile.name, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.send('Le fichier a été soumis avec succès !');
+  });
+});
+
+// Démarrer le serveur
+app.listen(3000, () => {
+  console.log('Le serveur est lancé sur le port 3000.');
+});
